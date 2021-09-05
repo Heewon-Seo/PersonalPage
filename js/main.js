c6324606034bb76de229f1c93a650ce4eeb6aa98
Heewon-Seo <heewonseo.dev@gmail.com>
2021-09-06 03:29:10 +0900

    메뉴바 색깔 수정

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ photo.addEventListener('click',function (e) {
     e.preventDefault();
     window.scrollTo({top:photoLocation,behavior:'smooth'});
 })
+
+window.scrollY;
+document.scrollingElement.scrollTop;
+
+window.addEventListener('scroll',()=>{
+    let scrollLocation = document.documentElement.scrollTop;
+    let windowHeight = window.innerHeight;
+    const section = document.querySelector("#memories").scrollHeight;
+
+    if(scrollLocation-section>400) {
+        home.style.color = "#607d8b";
+        photo.style.color = "#607d8b";
+    } else {
+        home.style.color = "#C8C6C6";
+        photo.style.color = "#C8C6C6";
+    }
+
+});
